8c80aa6e74c40bd80726068fe510be0a8b39d6ed
landsreyk <mr.andrey.kozyrev@gmail.com>
2025-12-28 10:34:34 +0300

    updated from develop_2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world");
+        System.out.println("=".repeat(100));
+        System.out.println("Hello, world!");
+        System.out.println("=".repeat(100));
     }
 }
