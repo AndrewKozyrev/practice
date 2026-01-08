fd965fb529ff1ec13648eaf1f7ded231ed2497ca
landsreyk <mr.andrey.kozyrev@gmail.com>
2026-01-08 18:52:30 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        IndexSearch search = new IndexSearch();
-        assert search.indexOf(new int[]{1, 2, 4, 4, 5, 4, 9, 8, 11, 13, 0}, 4, 3, 7) == 6;
+
     }
 }
